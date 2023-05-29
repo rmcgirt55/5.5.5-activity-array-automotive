09f55c21e191c0a507106705c083556c3c50ce1d
Richard McGirt <53379981+rmcgirt55@users.noreply.github.com>
2023-05-29 15:32:51 -0400

    completed mercurySedan.js

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,35 +1,68 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
-
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
-
-
-//After you write the derived Car class, you should test it out.
-
-//Note: You can code your derived Car class here or make a file named index.js and do it there.
-
-
-//TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
 
 
+//
+
+const VehicleModule = require('./vehicle').Vehicle
+
+class Car extends VehicleModule{
+    constructor(make, model, year, color, mileage){
+    super(make, model, year, color, mileage);
+    this.maximumPassengers = 5;
+    this.passenger = 0;
+    this.numberOfWheels = 4; 
+    this.maximumSpeed = 160;
+    this.fule = 10;
+    this.scheduleService = false;
+    }
+    loadPassenger(num){
+        this.passenger = num 
+        this.passenger <= this.maximumPassengers ? console.log("their is avalable room") : console.log("Not enough room for all passengers");
+    }
+    start(){
+        this.fule > 0 ? console.log("The car starts") : console.log("The car wont start")
+    }
+    //does the car neede to be serviced
+    checkService(){
+        this.mileage >= 30000 ? this.scheduleService = true : this.scheduleService = false
+        console.log(this.scheduleService)
+    }
+}
+
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
+
+// new truck class that extends Vehicle
+class Truck extends VehicleModule{
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = 7;
+        this.passenger = 0;
+        this.numberOfWheels = 4; 
+        this.maximumSpeed = 100;
+        this.fule = 10;
+        this.scheduleService = false;
+        this.allTerrain = true
+    }
+    //Is this an all wheel drive 
+    driveType(){
+        this.allTerrain == true ? console.log("This vehicle has all wheel drive") : console.log("This vehicle has rear wheel drive")
+    }
+    //should the driver turn on overdrive
+    overDrive(speed){
+        speed > this.maximumSpeed ? console.log("Turn on the overdrive") : ("No overdrive needed")
+    }
+}
+let F150 = new Truck ('Ford', 'F150', 2020, 'Red', 50000)
+
+F150.driveType()
+F150.overDrive(130)
+F150.typeOfVehicle()
 
 
-
-
-
-
-
-
-
-//TO DO: Creating Instances and Testing Them
-
-//You can use the same instance "v" of the Vehicle class above for the base class.
-
-
-
-
-
-//Create at least two new instances of the Car class and test them here:
